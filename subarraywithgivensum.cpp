999d0811f6eb480861e495a96f7c2447c2dd88fa
jaiswalatul <jaiswalatul018@gmail.com>
2023-12-06 22:44:53 +0530

    method 3

--- subarraywithgivensum.cpp
+++ subarraywithgivensum.cpp
@@ -1,5 +1,5 @@
 
-// <***************method-1*******************>
+// <***************method-1*******************> Partiallly accepted
 
 
 #include <bits/stdc++.h> 
@@ -21,7 +21,7 @@ int getLongestSubarray(vector<int>& nums, int k){
 }
 
 
-// <***************Method-2*******************>
+// <***************Method-2*******************>  Partialy accepted
 
 
 // int getLongestSubarray(vector<int>& a, int k){
@@ -48,5 +48,36 @@ int getLongestSubarray(vector<int>& nums, int k){
 // }
 
 
-//************************ Method-2*********************
+//************************ Method-3*********************
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    int left=0;
+    int right=0;
+    long long sum=a[0];
+    int maxlen=0;
+    int n=a.size();
+
+    while(right<n){
+        while(left<=right && sum>k){
+            sum-=a[left];
+            left++;
+        }
+
+        if(sum==k){
+            maxlen=max(maxlen,right-left+1);
+        }
+
+        right++;
+        if(right<n){
+            sum+=a[right];
+        }
+    
+    }
+
+    return maxlen;
+
+}
+
+
 
