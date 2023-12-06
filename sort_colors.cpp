39046f022619ae102afa2381f669457010a35e8b
jaiswalatul <jaiswalatul018@gmail.com>
2023-12-06 22:58:09 +0530

    wed-2

---
+++ sort_colors.cpp
@@ -0,0 +1,25 @@
+// Method-1
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int a=0;
+        int c=-1;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+              for(int j=0;j<n;j++){
+                  if(a==nums[j]){
+                      c++;
+                      swap(nums[i+c],nums[j]);
+                  }
+
+                  if(j==n-1){
+                      a++;
+                      i=i+c;
+                      c=-1;
+                  }
+              }
+        }
+    }
+};
+
+//  Method-2
