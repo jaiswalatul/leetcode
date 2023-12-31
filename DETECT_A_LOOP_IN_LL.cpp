07c415fcd98c52f62c6744c1248105f7b8ddbeba
jaiswalatul <jaiswalatul018@gmail.com>
2023-12-31 22:58:35 +0530

    new

---
+++ DETECT_A_LOOP_IN_LL.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+//*****************Method-2***********8
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*>s;
+        for(ListNode *curr=head;curr!=nullptr;curr=curr->next){
+            if(s.find(curr)!=s.end()){
+                return true;
+            }
+            s.insert(curr);
+        }
+        return false;
+    }
+};
